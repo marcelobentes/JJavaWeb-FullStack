fd4137091450962d964487e49c9622becf883d99
marcelobentes <marcelo.maia.m@gmail.com>
2021-09-18 21:33:10 -0300

    mudando conversão de String para Double.parseDouble

--- src/main/AlunoTest.java
+++ src/main/AlunoTest.java
@@ -7,9 +7,8 @@ import model.Aluno;
 public class AlunoTest {
 
 	public static void main(String[] args) {
-		/*Objero ainda não existe na memoria*/
-		@SuppressWarnings("unused")
-		Aluno aluno1;
+		/*Objeto ainda não existe na memoria*/
+		//Aluno aluno1;
 		
 		/*Agora temos um objeto real na memoria*/
 		Aluno aluno2 = new Aluno(null, 0, null, null, null, null, null, null, null, null);
@@ -24,24 +23,24 @@ public class AlunoTest {
 		String rg = JOptionPane.showInputDialog("Informe o RG: ");
 		String serie = JOptionPane.showInputDialog("Informe a serie: ");
 		String nota1 = JOptionPane.showInputDialog("Informe o nota1: ");
-		String nota2 = JOptionPane.showInputDialog("Informe o nota1: ");
-		String nota3 = JOptionPane.showInputDialog("Informe o nota1: ");
-		String nota4 = JOptionPane.showInputDialog("Informe o nota1: ");
+		String nota2 = JOptionPane.showInputDialog("Informe o nota2: ");
+		String nota3 = JOptionPane.showInputDialog("Informe o nota3: ");
+		String nota4 = JOptionPane.showInputDialog("Informe o nota4: ");
 		
 		aluno2.setNome(nome);
 		aluno2.setNomeEscola(escola);
 		aluno2.setNomeMae(mae);
 		aluno2.setNomePai(pai);
-		aluno2.setIdade(Integer.valueOf(idade));
+		aluno2.setIdade(Integer.parseInt(idade));
 		aluno2.setDataNascimento(dataNascimento);
 		aluno2.setDataMatricula(matricula);
 		aluno2.setNumeroCpf(cpf);
 		aluno2.setRg(rg);
 		aluno2.setSerieMatriculado(serie);
-		aluno2.setNota1(Double.valueOf(nota1));
-		aluno2.setNota2(Double.valueOf(nota2));
-		aluno2.setNota3(Double.valueOf(nota3));
-		aluno2.setNota4(Double.valueOf(nota4));
+		aluno2.setNota1(Double.parseDouble(nota1));
+		aluno2.setNota2(Double.parseDouble(nota2));
+		aluno2.setNota3(Double.parseDouble(nota3));
+		aluno2.setNota4(Double.parseDouble(nota4));
 		System.out.println("O nome do Aluno é "+aluno2.getNome());
 		System.out.println("Nome da escola: "+aluno2.getNomeEscola());
 		System.out.println("Idade: "+aluno2.getIdade());
