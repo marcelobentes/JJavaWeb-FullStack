1ad298b1d09168b14100c75a02eaadb7a15c65b5
marcelobentes <marcelo.maia.m@gmail.com>
2021-09-20 21:50:47 -0300

    Criando a classe Disciplina

--- src/main/AlunoTest.java
+++ src/main/AlunoTest.java
@@ -22,9 +22,13 @@ public class AlunoTest {
 		String cpf = JOptionPane.showInputDialog("Informe o CPF: ");
 		String rg = JOptionPane.showInputDialog("Informe o RG: ");
 		String serie = JOptionPane.showInputDialog("Informe a serie: ");
+		String disciplina1 = JOptionPane.showInputDialog("Informe a disciplina1: ");
 		String nota1 = JOptionPane.showInputDialog("Informe o nota1: ");
+		String disciplina2 = JOptionPane.showInputDialog("Informe a disciplina2: ");
 		String nota2 = JOptionPane.showInputDialog("Informe o nota2: ");
+		String disciplina3 = JOptionPane.showInputDialog("Informe a disciplina3: ");
 		String nota3 = JOptionPane.showInputDialog("Informe o nota3: ");
+		String disciplina4 = JOptionPane.showInputDialog("Informe a disciplina4: ");
 		String nota4 = JOptionPane.showInputDialog("Informe o nota4: ");
 		
 		aluno2.setNome(nome);
@@ -37,10 +41,15 @@ public class AlunoTest {
 		aluno2.setNumeroCpf(cpf);
 		aluno2.setRg(rg);
 		aluno2.setSerieMatriculado(serie);
-		aluno2.setNota1(Double.parseDouble(nota1));
-		aluno2.setNota2(Double.parseDouble(nota2));
-		aluno2.setNota3(Double.parseDouble(nota3));
-		aluno2.setNota4(Double.parseDouble(nota4));
+		aluno2.getDisciplina().setDisciplina1(disciplina1);
+		aluno2.getDisciplina().setDisciplina2(disciplina2);
+		aluno2.getDisciplina().setDisciplina3(disciplina3);
+		aluno2.getDisciplina().setDisciplina4(disciplina4);
+		
+		aluno2.getDisciplina().setNota1(Double.parseDouble(nota1));
+		aluno2.getDisciplina().setNota2(Double.parseDouble(nota2));
+		aluno2.getDisciplina().setNota3(Double.parseDouble(nota3));
+		aluno2.getDisciplina().setNota4(Double.parseDouble(nota4));
 		System.out.println("O nome do Aluno é "+aluno2.getNome());
 		System.out.println("Nome da escola: "+aluno2.getNomeEscola());
 		System.out.println("Idade: "+aluno2.getIdade());
--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -11,10 +11,8 @@ public class Aluno {
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculado;
-	private double nota1;
-	private double nota2;
-	private double nota3;
-	private double nota4;
+	
+	Disciplina disciplina = new Disciplina();
 	
 	
 	public Aluno(String nome, int idade, String dataNascimento, String rg, String numeroCpf, String nomeMae,
@@ -30,6 +28,10 @@ public class Aluno {
 		this.dataMatricula = dataMatricula;
 		this.nomeEscola = nomeEscola;
 		this.serieMatriculado = serieMatriculado;
+	}
+
+	public Aluno() {
+		// TODO Auto-generated constructor stub
 	}
 
 	public String getNome() {
@@ -111,41 +113,21 @@ public class Aluno {
 	public void setSerieMatriculado(String serieMatriculado) {
 		this.serieMatriculado = serieMatriculado;
 	}
-
-	public double getNota1() {
-		return nota1;
-	}
-
-	public void setNota1(double nota1) {
-		this.nota1 = nota1;
-	}
-
-	public double getNota2() {
-		return nota2;
-	}
-
-	public void setNota2(double nota2) {
-		this.nota2 = nota2;
-	}
-
-	public double getNota3() {
-		return nota3;
-	}
-
-	public void setNota3(double nota3) {
-		this.nota3 = nota3;
-	}
-
-	public double getNota4() {
-		return nota4;
-	}
-
-	public void setNota4(double nota4) {
-		this.nota4 = nota4;
-	}
 	
+	
+	
+	
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+
 	public double getMedianota() {
-		return (nota1 + nota2 + nota3 + nota4) /4;
+		return (disciplina.getNota1() + disciplina.getNota2()
+		+ disciplina.getNota3() + disciplina.getNota4()) /4;
 	}
 	
 	public boolean getResultado() {
@@ -157,4 +139,28 @@ public class Aluno {
 		}
 	}
 
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	
 }
